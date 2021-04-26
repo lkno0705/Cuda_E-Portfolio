885474fdd939919e695bfc08c99622c96d22da9c
lkno0705 <lkno0705@gmail.com>
2021-04-26 22:00:41 +0200

    added Windoof support

--- MatrixAddition/main.cu
+++ MatrixAddition/main.cu
@@ -1,3 +1,7 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
 #include <iostream>
 #include <chrono>
 #include <stdlib.h>
@@ -8,7 +12,7 @@ const int n = 14400;
 // CUDA Kernel for MatrixAddition
 __global__ void MatAdd(float* A, float* B, float* C) {
     int i = blockIdx.x * blockDim.x + threadIdx.x; // Calculates Current Index
-    if(i < m*n)
+    if (i < m*n)
         C[i] = A[i] + B[i];
 }
 
@@ -32,27 +36,23 @@ float* createEmptyMatrix(float* matrix, int m, int n) {
     return matrix;
 }
 
-void deleteMatrix(float** matrix) {
-    for(int r = 0; r < m; r++){
-        delete[] matrix[r];
-    }
-    delete[] matrix;
-}
-
 int main() {
-
-    float* A;
-    float* B;
-    float* C;
+    float* A = (float*)malloc(m * n);
+    float* B = (float*)malloc(m * n);
+    float* C = (float*)malloc(m * n);
 
     float* d_A;
     float* d_B;
     float* d_C;
 
     auto start1 = std::chrono::high_resolution_clock::now();
+    std::cout << "[+] Generation of Matrices started \n";
     A = createRandomMatrix(A, m, n);
+    std::cout << "[+] Generation of Matrix A finished \n";
     B = createRandomMatrix(B, m, n);
+    std::cout << "[+] Generation of Matrix B finished \n";
     C = createEmptyMatrix(C, m, n);
+    std::cout << "[+] Generation of Matrix C finished \n";
     auto stop1 = std::chrono::high_resolution_clock::now();
 
     std::cout << "[+] Generation on CPU finished \n[+] Duration: " << std::chrono::duration<double>(stop1 - start1).count() << " seconds\n";
